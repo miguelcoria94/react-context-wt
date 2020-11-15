b8b62c104d2b183c676ecb8fc171463b5bccd3bd
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 04:25:22 -0600

    import images

--- react-context-pups-starter/src/App.js
+++ react-context-pups-starter/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import speedy from './pups/speedy-pup.jpg'
+import banana from './pups/banana-pup.jpg'
+import sleepy from './pups/sleepy-pup.jpg'
 
 function App() {
   return (
