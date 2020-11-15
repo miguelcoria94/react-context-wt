ac57c7e52712f5adbf03e0cee2ad04baa7b42f70
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 04:39:25 -0600

    added context

--- react-context-pups-starter/src/App.js
+++ react-context-pups-starter/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import speedy from './pups/speedy-pup.jpg'
-import banana from './pups/banana-pup.jpg'
-import sleepy from './pups/sleepy-pup.jpg'
+import PupContext from './PupContext';
+import speedy from './pups/speedy-pup.jpg';
+import banana from './pups/banana-pup.jpg';
+import sleepy from './pups/sleepy-pup.jpg';
 
-const App = () => (
+
+
+const App = ({ puppyType }) => (
   <div id="pup">
-    <img src={speedy} alt='pup' />
+    <img src={puppyType} alt='pup' />
   </div>
 )
+
+class AppWithContext extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      puppyType: speedy,
+    }
+  }
+
+  render() {
+    return (
+      <PupContext.Provider value={this.state}>
+        <App puppyType={this.state.puppyType} />
+      </PupContext.Provider>
+    )
+  }
+}
 
 export default App;
